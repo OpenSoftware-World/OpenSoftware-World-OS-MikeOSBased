706746b3b1c375f529abea85330f6bc01a474afd
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-05-12 22:16:09 +0300

    OpenSoftware-World OS (Based on MikeOS) 1.3 (Redesign)

--- Boot/boot.asm
+++ Boot/boot.asm
@@ -314,7 +314,7 @@ l2hts:			; Calculate head, track and sector settings for int 13h
 ; ------------------------------------------------------------------
 ; STRINGS AND VARIABLES
 
-	kern_filename	db "KERNEL  BIN"	; OpenSoftware-World OS (MikeOSBased) kernel filename
+	kern_filename	db "KERNEL  BIN"	; OpenSoftware-World OS kernel filename
 
 	disk_error	db "Floppy error! Press any key...", 0
 	file_not_found	db "KERNEL.BIN not found!", 0
